55229febaff06bb5671aa902cc183b9f8bb49143
z3DeL <87988350+z3DeL@users.noreply.github.com>
2021-10-22 01:01:56 +0500

    Update 2.cpp

--- zadanie3/2.cpp
+++ zadanie3/2.cpp
@@ -1,19 +1,35 @@
+//#include <iostream> из десятичной в двоичную
+
+//using namespace std;
+//void rec(int a){
+//        if (a  == 0 || a == 1){
+//            cout<< a<<endl;}
+//            else {
+//            rec(a / 2);
+//            cout<< a % 2<<endl;
+//            }
+//         }
+//
+//int main()
+//{
+//    int a;
+//    cin >>a;
+//    rec(a);
+//    return 0;
+//}
 #include <iostream>
-
+// из двоичной в десятичную
 using namespace std;
-void rec(int a){
-        if (a  == 0 || a == 1){
-            cout<< a<<endl;}
-            else {
-            rec(a / 2);
-            cout<< a % 2<<endl;
-            }
-         }
-
+void rec(int a, int sum,int k){ if (a % 10 == a){ cout<< a  * k + sum;}
+   else  {rec(a / 10, sum + (a % 10) * k, k * 2);
+   }
+}
+void conv(int n){
+rec(n,0,1);}
 int main()
 {
-    int a;
-    cin >>a;
-    rec(a);
-    return 0;
+int b;
+cin >>b;
+conv(b);
+return 0;
 }
