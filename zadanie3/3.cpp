934be3d424146f6643556aef138f60b2b34c9b32
z3DeL <87988350+z3DeL@users.noreply.github.com>
2021-10-21 22:22:36 +0500

    Create 3.cpp

---
+++ zadanie3/3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+void rec(int a,int del){
+        if (a % del == 0){
+            cout<< del<<endl;
+            return rec(a / del, del);}
+            else { return rec(a, del +1);}
+         }
+void prime(int n){
+rec(n,2);}
+int main()
+{
+    int a;
+    cin >>a;
+    prime(a);
+    return 0;
+}
