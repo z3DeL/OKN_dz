1c6d02d9f9b735cd57db426313a16d4f969b052d
z3DeL <87988350+z3DeL@users.noreply.github.com>
2021-12-16 12:56:48 +0300

    Update 4.cpp

--- zadanie6/4.cpp
+++ zadanie6/4.cpp
@@ -4,49 +4,50 @@
 
 using namespace std;
 
+void delete_array(int** array, int n) {
+	for (int i = 0; i < n; i++) {
+		delete[] array[i];
+	}
+	delete[] array;
+}
+
 void print_array(int**array, int n, int m) {
 
 for (int i = 0; i < n; i++) {
 
-for (int j = 0; j < m;j++) {
+    for (int j = 0; j < m;j++) {
 
-cout << array[i][j] << " ";
+    cout << array[i][j] << " ";
 
-}cout << endl;
+    }cout << endl;
 
-}
-
-}
+}}
 
 void random_array(int **array, int n, int m) {
 
 for (int i = 0; i < n; i++) {
 
-for (int j = 0; j < m;j++) {
+    for (int j = 0; j < m;j++) {
 
-array[i][j] = rand()%90+10;
+    array[i][j] = rand()%90+10;
 
-}
-
-}
-
-}
+}}}
 
 
 
 void perest(int **array, int n){
 int i_min = 0, j_min = 0;
 
-
 for (int i = 0; i < n; i++) {
 
-for (int j = 0; j < n;j++) {
+    for (int j = 0; j < n;j++) {
 
-if ((array[i][j] < array[i_min][j_min] && array[i][j]>0) || array[i_min][j_min]<=0 ) {
+    if ((array[i][j] < array[i_min][j_min] && array[i][j]>0) || array[i_min][j_min]<=0 ) {
 
-i_min = i; j_min = j;
+    i_min = i; j_min = j;
 
 }}}
+
 for (int i = 0; i < n;i++){
     int temp = array[i][j_min];
     array[i][j_min]=array[i_min][i];
@@ -77,5 +78,6 @@ print_array(mas, n, n);
 cout<<endl;
 perest(mas,n);
 print_array(mas,n,n);
+delete_array(mas,n);
 return 0;
 }
